1a1711f40767e24b958a734679113bc8cfafe58f
SalahShehada <salah.shehade12@gmail.com>
2025-03-23 16:37:01 +0200

    complete

---
+++ Majority Element/MajorityElement.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        int candidate = nums[0], count = 0;
+
+        foreach (int num in nums)
+        {
+            if (count == 0)
+            {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
